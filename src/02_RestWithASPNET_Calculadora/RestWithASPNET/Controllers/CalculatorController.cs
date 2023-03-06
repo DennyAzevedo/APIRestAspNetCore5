50f2bb9f92adacd062d882033bd364754ba15544
Denny Azevedo <denny.filho@animaeducacao.com.br>
2023-03-06 16:47:39 -0300

    Iniciando a Calculadora

---
+++ src/02_RestWithASPNET_Calculadora/RestWithASPNET/Controllers/CalculatorController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RestWithASPNET.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber) {
+			if (IsNumeric(firstNumber) && IsNumeric(secondNumber)) {
+				var sum = ConvetToDecimal(firstNumber) + ConvetToDecimal(secondNumber);
+				return Ok(sum.ToString());
+			}
+			return BadRequest("Invalid Input");
+		}
+
+		private decimal ConvetToDecimal(string number) {
+			throw new NotImplementedException();
+		}
+
+		private bool IsNumeric(string number) {
+			throw new NotImplementedException();
+		}
+	}
+}
